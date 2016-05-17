96c3399f7f3b6d310e1b2737c365f0c7e44aed10
fuzongjian <2917066078@qq.com>
2016-05-17 18:05:43 +0800

    初始化工程

---
+++ CustomTabBar_Swift/CustomTabBar.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTabBar.swift
+//  CustomTabBar_Swift
+//
+//  Created by 陈舒澳 on 16/5/17.
+//  Copyright © 2016年 speeda. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBar: UITabBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CustomTabBar_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTabBar_Swift
+//
+//  Created by 陈舒澳 on 16/5/17.
+//  Copyright © 2016年 speeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
